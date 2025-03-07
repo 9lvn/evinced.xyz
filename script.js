ad3f46098235e26ec477ec30118745ea96df085d
evinced <123937949+9lvn@users.noreply.github.com>
2025-03-07 14:05:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+let stickman = {
+    x: 100,
+    y: 100,
+    radius: 10,
+    color: 'black',
+    draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+};
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    stickman.draw();
+    requestAnimationFrame(draw);
+}
+
+draw();
